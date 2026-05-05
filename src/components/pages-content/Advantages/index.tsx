6c8b4662643161d3ec8f83f95f9b27978efa83a1
Denis <denisholmov@mail.ru>
2026-05-05 20:37:04 +0300

    сделан блок advantages

--- src/components/pages-content/Advantages/index.tsx
+++ src/components/pages-content/Advantages/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles/index.module.css";
+
 export const Advantages = () => {
   return (
     <section id="advantages" className={styles.advantages}>
@@ -9,8 +10,8 @@ export const Advantages = () => {
             <div className={styles.image}>
                 <img src="./public/images/cifra-mini.png" alt="Обучающий набор" />
             </div>
-            <p>Обучающий набор для получения практических навыков 
-интеграции технического зрения в реальное «железо».</p>
+            <p className={styles.description}>Обучающий набор для получения практических навыков 
+интеграции технического зрения в <br /> реальное «железо».</p>
         </div>
 
         <ul className={styles.list}>
@@ -18,29 +19,37 @@ export const Advantages = () => {
                 <div className={styles.icon}>
                     <img src="./public/images/book.png" alt="Методическое сопровождение" />
                 </div>
-                <h3>Методическое сопровождение</h3>
-                <p>Интересная обучающая программа, поддержка преподавателей и быстрое внедение в учебный процесс.</p>
+               <div className={styles.itemContent}>
+               <h3>Методическое сопровождение</h3>
+               <p>Интересная обучающая программа, поддержка преподавателей и быстрое внедение в учебный процесс.</p>
+               </div>
             </li>
             <li className={styles.item}>
                 <div className={styles.icon}>
                     <img src="./public/images/cup.png" alt="Рыночная ценность" />
                 </div>
+                <div className={styles.itemContent}>
                 <h3>Рыночная ценность</h3>
                 <p>Привлекательные для молодежи современные технологии с мотивирующим наглядным результатом.</p>
+                </div>
             </li>
             <li className={styles.item}>
                 <div className={styles.icon}>
                     <img src="./public/images/tools.png" alt="Востребованный стек" />
                 </div>
+                <div className={styles.itemContent}>
                 <h3>Востребованный стек</h3>
                 <p>Обучение строится на мировых индустриальных стандартах – Linux, Python, OpenCV. </p>
+                </div>
             </li>
             <li className={styles.item}>
                 <div className={styles.icon}>
                     <img src="./public/images/neurons.png" alt="Готовая образовательная среда" />
                 </div>
+                <div className={styles.itemContent}>
                 <h3>Готовая образовательная среда</h3>
                 <p>Система развернута, драйвера установлены, библиотека собрана – студентам осталось только творить.</p>
+                </div>
             </li>
         </ul>
       </div>
